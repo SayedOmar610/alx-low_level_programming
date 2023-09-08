7843f6d1545bd4a1d340e7208e82f8e35479f2b3
sayedomar <sayeed.omeer22@gmail.com>
2023-09-08 13:34:34 -0700

    adding corrected code for poisitive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -23,4 +25,5 @@ int main(void)
         printf("%d is negative \n",n);
         }
         return (0);
+
 }
