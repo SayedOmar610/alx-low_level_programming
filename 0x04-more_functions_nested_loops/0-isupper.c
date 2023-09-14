1773f7710feb4fa0a0bab58e930b78c9c9c06665
sayedomar <sayeed.omeer22@gmail.com>
2023-09-14 03:10:56 -0700

    adding

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: returned character from function
+ *
+ * Return: 1 for upper case, 0 elsewise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this directory is for  0x04-more functions nested loops projects
