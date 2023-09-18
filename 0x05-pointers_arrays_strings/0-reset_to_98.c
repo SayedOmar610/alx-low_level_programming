cbc385071eccc0ae3fe4a25655dce1d9ee231e1b
sayedomar <sayeed.omeer22@gmail.com>
2023-09-18 12:34:43 -0700

    adding first lesson in pointrs

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - akes a pointer to an int as parameter and updates
+ * the value it points to to 98
+ *
+ * @n: parameter of function
+ *
+ * Return: value of parameter n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
