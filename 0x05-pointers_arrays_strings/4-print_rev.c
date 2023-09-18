c2429273eec23eb470acd1a6f815b2eb210ec84b
sayedomar <sayeed.omeer22@gmail.com>
2023-09-18 14:10:23 -0700

    adding

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,27 +1,25 @@
 #include "main.h"
-
 /**
  * print_rev - imprime en reversa
- *
  * @s: string
- *
  * return: 0
  */
 void print_rev(char *s)
 {
-	int count  = 0;
-	int rvs;
+	int longi = 0;
+	int o;
 
 	while (*s != '\0')
 	{
-		count++;
-		S++;
+		longi++;
+		s++;
 	}
-	S--
-	for (rvs = count; rvs > 0; rvs--)
+	s--;
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
-		S--;
+		s--;
 	}
+
 	_putchar('\n');
 }
