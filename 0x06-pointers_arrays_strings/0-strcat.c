1c6664d815c3a636e9f832dc680f5f0524907112
sayedomar <sayeed.omeer22@gmail.com>
2023-09-20 12:56:37 -0700

    adding

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	i == 0;
 	while (dest[i] != '\0')
 		i++;
-	for (j = 0; src[j] != '\0';j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
