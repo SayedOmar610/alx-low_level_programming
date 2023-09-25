3ad85eef6eb5153ac3a7fd8036beb367bf754d64
sayedomar <sayeed.omeer22@gmail.com>
2023-09-25 09:51:42 -0700

    adding new function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * memset - fills the first n bytes of the memory area pointed
+ * to by s with the constant byte b
+ *
+ * @s: pointer variable of tyoe char
+ * @n: integer variable
+ * @b: constant byte of type char
+ *
+ * Return: pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int k;
+
+	for (k = 0; n > 0; K++)
+	{
+		s[k] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+this directory is for 0x07. C - Even more pointers, arrays and strings
+C
