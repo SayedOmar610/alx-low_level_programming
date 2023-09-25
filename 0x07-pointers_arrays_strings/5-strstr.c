e967ce51933ff1d695f7abe155ab54f88c83c948
sayedomar <sayeed.omeer22@gmail.com>
2023-09-25 13:16:57 -0700

    adding

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of byteschar
+ * _strstr - locates a substring
  *
- * @s: pointer to string
- * @accept: string to be compared
+ * @haystack: string
+ * @needle: substring
  *
  * Return: Always 0 (Success)
  */
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
-	
-	i = 0;
-	while (s[i] != '\0')
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			for (j = 0; accept[j]; j++)
-		{
-			if (s[i] == accept[j])
-				return (&s[i]);
+			if (needle[j] == haystack[i])
+			{
+				return (&haystack[i]);
+			}
 		}
-		i++;
-		}
+	}
 	return (0);
 }
