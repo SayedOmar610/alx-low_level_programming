9a9e3d20ffed4f3a9e8caf9cdf16ac1c31af3312
sayedomar <sayeed.omeer22@gmail.com>
2023-10-19 14:42:07 -0700

    adding

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%u] %s\n", _strlen(h->str), h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
