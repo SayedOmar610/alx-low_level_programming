361c1b4a710ff07d914a003435f1652e03107c81
sayedomar <sayeed.omeer22@gmail.com>
2023-10-31 11:39:07 -0700

    adding

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	check_file(file_from, file_to, argv);
 	ch_num = 1024;
 	while (ch_num == 1024)
